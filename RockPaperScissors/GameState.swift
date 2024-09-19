1dccaff423349b0a22d8c12b3f750f5dd947b179
Sema Topcu <ssemaatopcu.@gmail.com>
2024-09-19 18:03:27 +0300

    defined signs and game states

---
+++ RockPaperScissors/GameState.swift
@@ -0,0 +1,15 @@
+//
+//  GameState.swift
+//  RockPaperScissors
+//
+//  Created by Sema Topcu on 9/19/24.
+//
+
+import Foundation
+
+enum GameState {
+    case start
+    case win
+    case lose
+    case draw
+}
---
+++ RockPaperScissors/Sign.swift
@@ -0,0 +1,14 @@
+//
+//  Sign.swift
+//  RockPaperScissors
+//
+//  Created by Sema Topcu on 9/19/24.
+//
+
+import Foundation
+
+enum Sign {
+    case rock
+    case paper
+    case scissors
+}

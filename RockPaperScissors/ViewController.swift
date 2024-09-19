be23303a909bdf1fec4a9e3a3ea84ade02b98440
Sema Topcu <ssemaatopcu.@gmail.com>
2024-09-19 19:50:41 +0300

    edited UI elements and made connections

--- RockPaperScissors/ViewController.swift
+++ RockPaperScissors/ViewController.swift
@@ -9,6 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var appSign: UILabel!
+    @IBOutlet var gameStatus: UILabel!
+    @IBOutlet var rockSign: UIButton!
+    @IBOutlet var paperSign: UIButton!
+    @IBOutlet var scissorsSign: UIButton!
+    
+    @IBAction func again(_ sender: Any) {
+    }
+    @IBAction func playerRock(_ sender: Any) {
+    }
+    @IBAction func playerPaper(_ sender: Any) {
+    }
+    @IBAction func playerScissors(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.

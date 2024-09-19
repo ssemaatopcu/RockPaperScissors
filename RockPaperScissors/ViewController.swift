d2abb8ea1c2f2b49f9d068982de4881ee8e82fcc
Sema Topcu <ssemaatopcu.@gmail.com>
2024-09-19 20:09:36 +0300

    updated UI based on game state

--- RockPaperScissors/ViewController.swift
+++ RockPaperScissors/ViewController.swift
@@ -14,14 +14,44 @@ class ViewController: UIViewController {
     @IBOutlet var rockSign: UIButton!
     @IBOutlet var paperSign: UIButton!
     @IBOutlet var scissorsSign: UIButton!
+    @IBOutlet var againButton: UIButton!
     
     @IBAction func again(_ sender: Any) {
+        updateUI(for: .start)
     }
     @IBAction func playerRock(_ sender: Any) {
     }
     @IBAction func playerPaper(_ sender: Any) {
     }
     @IBAction func playerScissors(_ sender: Any) {
+    }
+    
+    func updateUI(for state: GameState) {
+        gameStatus.text = state.message
+        
+        switch state {
+        case .start:
+            view.backgroundColor = UIColor.systemGray
+        case .win:
+            view.backgroundColor = UIColor.systemGreen
+        case .lose:
+            view.backgroundColor = UIColor.systemRed
+        case .draw:
+            view.backgroundColor = UIColor.systemYellow
+        }
+        
+        if state == .start {
+            appSign.text = "🤖"
+            againButton.isHidden = true
+            
+            rockSign.isHidden = false
+            paperSign.isHidden = false
+            scissorsSign.isHidden = false
+            
+            rockSign.isEnabled = true
+            paperSign.isEnabled = true
+            scissorsSign.isEnabled = true
+        }
     }
     
     override func viewDidLoad() {

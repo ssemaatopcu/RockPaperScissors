9b7d4f1ba0245ec82caa347bab9e465c6c68d691
Sema Topcu <ssemaatopcu.@gmail.com>
2024-09-19 19:20:44 +0300

    added instance method to compare game signs

--- RockPaperScissors/Sign.swift
+++ RockPaperScissors/Sign.swift
@@ -11,4 +11,15 @@ enum Sign {
     case rock
     case paper
     case scissors
+    
+    func compare(opponent: Sign) -> GameState {
+        switch (self, opponent) {
+                case (.rock, .scissors), (.scissors, .paper), (.paper, .rock):
+                    return .win
+                case (.rock, .paper), (.scissors, .rock), (.paper, .scissors):
+                    return .lose
+                default:
+                    return .draw
+                }
+    }
 }
